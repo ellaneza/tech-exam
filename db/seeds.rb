4f2cbd131269f6d0d72b7220ec8972fe9b9d1b7f
Emir LLaneza <emirllaneza@gmail.com>
2023-02-05 12:49:37 +0800

    Created seeder for covid_observation table

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,22 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'csv'
+
+csv_data = File.read(Rails.root.join('lib/covid_19_data.csv'))
+data_from_csv = CSV.parse(csv_data, headers: true, encoding: 'utf-8')
+covid_observations = data_from_csv.map do |row|
+  raw_data = {}
+  raw_data[:id] = row[row.headers.first]
+  raw_data[:observation_date] = Date.strptime(row['ObservationDate'], '%m/%d/%Y')
+  raw_data[:province] = row['Province/State']
+  raw_data[:country] = row['Country/Region']
+  raw_data[:last_updated] = DateTime.strptime(row['LastUpdate'],
+      '%m/%d/%Y %H:%M') rescue DateTime.parse(row['LastUpdate'])
+  raw_data[:confirmed] = row['Confirmed']
+  raw_data[:deaths] = row['Deaths']
+  raw_data[:recovered] = row['Recovered']
+  raw_data[:created_at] = DateTime.current
+  raw_data[:updated_at] = DateTime.current
+  raw_data
+end
+
+CovidObservation.upsert_all(covid_observations)
+puts "#{ CovidObservation.count } record has been pushed"
